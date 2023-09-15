4a253a5b39297ef0b0370ac4607d7b1054d81f93
kerlorant <kertesz.lorant@gmail.com>
2023-09-16 01:36:07 +0200

    20230916

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-function showContent() {
-    let temp = document.getElementsByTagName("template")[0];
-    let clon = temp.content.cloneNode(true);
-    document.body.appendChild(clon);
-  }
+var visitorCount = 0;
+
+// A látogatók számának növelése minden alkalommal, amikor valaki meglátogatja az oldalt
+
+window.onload = function() {
+  visitorCount++;
+};
+
+// A látogatók számának lekérése
+
+function getVisitorCount() {
+  return visitorCount;
+}
+// A látogatók számának lekérése
+
+var visitorCount = getVisitorCount();
+
+// A látogatók számának megjelenítése a `span` elemben
+
+document.querySelector("#visitor-count").textContent = visitorCount;
